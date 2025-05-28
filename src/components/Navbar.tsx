ee43415fd8945a53d84a795c1fc58fb333d80d65
Rafael Castro <ralphc1992@live.com>
2025-05-28 11:31:17 -0500

    new site

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,21 @@
 import logo from "../assets/logos/fast2lean.svg";
+import { RiMenu2Fill } from "react-icons/ri";
 
 export const Navbar: React.FC = () => {
   return (
     <>
       <div className=" bg-white w-full py-2 sticky top-0 z-60 shadow-md">
-        <div className="flex justify-between items-center max-w-[1400px] mx-auto px-4 lg:px-0">
+        <div className="flex justify-between items-center max-w-[1400px] mx-auto px-4">
           <img className="h-[15px]" src={logo} alt="" />
-          <ul className="flex gap-5 justify-between items-center">
+          <ul className="md:flex gap-5 justify-between items-center hidden">
             <li>Home</li>
             <li>Blog</li>
             <li>Resources</li>
             <li>Subscribe</li>
           </ul>
+          <div className="block md:hidden">
+            <RiMenu2Fill className="size-6" />
+          </div>
         </div>
       </div>
     </>
