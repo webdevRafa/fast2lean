d37b9f814206a9ceb4be6ac647992a7f481528a7
Rafael Castro <ralphc1992@live.com>
2025-06-16 09:13:24 -0500

    new

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -54,12 +54,12 @@ const ArticlePage: React.FC = () => {
             className="rounded-lg mb-6 w-full max-h-[400px] object-cover"
           />
         )}
-        <h1 className="text-3xl md:text-5xl font-bold mb-4">{article.title}</h1>
+        <h1 className="text-3xl md:text-4xl font-bold mb-4">{article.title}</h1>
         <p className="text-gray-600 text-sm mb-2">
           By {article.author} •{" "}
           {article.createdAt.toDate().toLocaleDateString()}
         </p>
-        <p className="text-lg text-gray-700 mb-10 italic">{article.excerpt}</p>
+        <p className="text-md text-gray-700 mb-10 italic">{article.excerpt}</p>
 
         <p className="text-gray-800 leading-relaxed whitespace-pre-line">
           <div
